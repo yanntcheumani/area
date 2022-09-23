a788d73474450506a716339bc3c56f1ccc64c3cd
le-gonz <antoine.beaudoux@epitech.eu>
2022-09-23 14:06:58 +0200

    [DELETE] all project frontend and dockerfile

---
+++ WebApp/.svelte-kit/runtime/env/static/private.js
@@ -0,0 +1,249 @@
+// this file is generated — do not edit it
+/** @type {import('$env/static/private').USER} */
+export const USER = "le-gonz";
+
+/** @type {import('$env/static/private').LC_TIME} */
+export const LC_TIME = "fr_FR.UTF-8";
+
+/** @type {import('$env/static/private').npm_config_user_agent} */
+export const npm_config_user_agent = "npm/8.12.2 node/v16.14.2 linux x64 workspaces/false";
+
+/** @type {import('$env/static/private').SSH_AGENT_PID} */
+export const SSH_AGENT_PID = "2074";
+
+/** @type {import('$env/static/private').XDG_SESSION_TYPE} */
+export const XDG_SESSION_TYPE = "x11";
+
+/** @type {import('$env/static/private').npm_node_execpath} */
+export const npm_node_execpath = "/usr/local/bin/node";
+
+/** @type {import('$env/static/private').SHLVL} */
+export const SHLVL = "1";
+
+/** @type {import('$env/static/private').npm_config_noproxy} */
+export const npm_config_noproxy = "";
+
+/** @type {import('$env/static/private').HOME} */
+export const HOME = "/home/le-gonz";
+
+/** @type {import('$env/static/private').OLDPWD} */
+export const OLDPWD = "/home/le-gonz/Epitech/tek3/DEV/B-DEV-500-BER-5-1-area-yann-arthur.tcheumani/Application";
+
+/** @type {import('$env/static/private').LESS} */
+export const LESS = "-R";
+
+/** @type {import('$env/static/private').DESKTOP_SESSION} */
+export const DESKTOP_SESSION = "ubuntu";
+
+/** @type {import('$env/static/private').npm_package_json} */
+export const npm_package_json = "/home/le-gonz/Epitech/tek3/DEV/B-DEV-500-BER-5-1-area-yann-arthur.tcheumani/WebApp/package.json";
+
+/** @type {import('$env/static/private').ZSH} */
+export const ZSH = "/home/le-gonz/.oh-my-zsh";
+
+/** @type {import('$env/static/private').LSCOLORS} */
+export const LSCOLORS = "Gxfxcxdxbxegedabagacad";
+
+/** @type {import('$env/static/private').npm_package_engines_node} */
+export const npm_package_engines_node = ">=16.7";
+
+/** @type {import('$env/static/private').GTK_MODULES} */
+export const GTK_MODULES = "gail:atk-bridge";
+
+/** @type {import('$env/static/private').GNOME_SHELL_SESSION_MODE} */
+export const GNOME_SHELL_SESSION_MODE = "ubuntu";
+
+/** @type {import('$env/static/private').PAGER} */
+export const PAGER = "less";
+
+/** @type {import('$env/static/private').MANAGERPID} */
+export const MANAGERPID = "1896";
+
+/** @type {import('$env/static/private').LC_MONETARY} */
+export const LC_MONETARY = "fr_FR.UTF-8";
+
+/** @type {import('$env/static/private').npm_config_userconfig} */
+export const npm_config_userconfig = "/home/le-gonz/.npmrc";
+
+/** @type {import('$env/static/private').npm_config_local_prefix} */
+export const npm_config_local_prefix = "/home/le-gonz/Epitech/tek3/DEV/B-DEV-500-BER-5-1-area-yann-arthur.tcheumani/WebApp";
+
+/** @type {import('$env/static/private').DBUS_SESSION_BUS_ADDRESS} */
+export const DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1000/bus";
+
+/** @type {import('$env/static/private').npm_config_engine_strict} */
+export const npm_config_engine_strict = "true";
+
+/** @type {import('$env/static/private').COLORTERM} */
+export const COLORTERM = "truecolor";
+
+/** @type {import('$env/static/private').COLOR} */
+export const COLOR = "1";
+
+/** @type {import('$env/static/private').npm_config_metrics_registry} */
+export const npm_config_metrics_registry = "https://registry.npmjs.org/";
+
+/** @type {import('$env/static/private').IM_CONFIG_PHASE} */
+export const IM_CONFIG_PHASE = "1";
+
+/** @type {import('$env/static/private').LOGNAME} */
+export const LOGNAME = "le-gonz";
+
+/** @type {import('$env/static/private')._} */
+export const _ = "/usr/local/bin/npm";
+
+/** @type {import('$env/static/private').JOURNAL_STREAM} */
+export const JOURNAL_STREAM = "8:51997";
+
+/** @type {import('$env/static/private').npm_config_prefix} */
+export const npm_config_prefix = "/usr/local";
+
+/** @type {import('$env/static/private').XDG_SESSION_CLASS} */
+export const XDG_SESSION_CLASS = "user";
+
+/** @type {import('$env/static/private').USERNAME} */
+export const USERNAME = "le-gonz";
+
+/** @type {import('$env/static/private').TERM} */
+export const TERM = "xterm-256color";
+
+/** @type {import('$env/static/private').npm_config_cache} */
+export const npm_config_cache = "/home/le-gonz/.npm";
+
+/** @type {import('$env/static/private').GNOME_DESKTOP_SESSION_ID} */
+export const GNOME_DESKTOP_SESSION_ID = "this-is-deprecated";
+
+/** @type {import('$env/static/private').WINDOWPATH} */
+export const WINDOWPATH = "2";
+
+/** @type {import('$env/static/private').npm_config_node_gyp} */
+export const npm_config_node_gyp = "/usr/local/lib/node_modules/npm/node_modules/node-gyp/bin/node-gyp.js";
+
+/** @type {import('$env/static/private').PATH} */
+export const PATH = "/home/le-gonz/Epitech/tek3/DEV/B-DEV-500-BER-5-1-area-yann-arthur.tcheumani/WebApp/node_modules/.bin:/home/le-gonz/Epitech/tek3/DEV/B-DEV-500-BER-5-1-area-yann-arthur.tcheumani/node_modules/.bin:/home/le-gonz/Epitech/tek3/DEV/node_modules/.bin:/home/le-gonz/Epitech/tek3/node_modules/.bin:/home/le-gonz/Epitech/node_modules/.bin:/home/le-gonz/node_modules/.bin:/home/node_modules/.bin:/node_modules/.bin:/usr/local/lib/node_modules/npm/node_modules/@npmcli/run-script/lib/node-gyp-bin:/home/le-gonz/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin";
+
+/** @type {import('$env/static/private').SESSION_MANAGER} */
+export const SESSION_MANAGER = "local/legonz-ThinkPad-L390:@/tmp/.ICE-unix/2112,unix/legonz-ThinkPad-L390:/tmp/.ICE-unix/2112";
+
+/** @type {import('$env/static/private').INVOCATION_ID} */
+export const INVOCATION_ID = "248f6781a3374752b36e1fa4b41e78d1";
+
+/** @type {import('$env/static/private').NODE} */
+export const NODE = "/usr/local/bin/node";
+
+/** @type {import('$env/static/private').npm_package_name} */
+export const npm_package_name = "sveltekit-static";
+
+/** @type {import('$env/static/private').GNOME_TERMINAL_SCREEN} */
+export const GNOME_TERMINAL_SCREEN = "/org/gnome/Terminal/screen/641e8255_8542_4455_a76f_8429fa2f5eda";
+
+/** @type {import('$env/static/private').XDG_MENU_PREFIX} */
+export const XDG_MENU_PREFIX = "gnome-";
+
+/** @type {import('$env/static/private').XDG_RUNTIME_DIR} */
+export const XDG_RUNTIME_DIR = "/run/user/1000";
+
+/** @type {import('$env/static/private').LC_ADDRESS} */
+export const LC_ADDRESS = "fr_FR.UTF-8";
+
+/** @type {import('$env/static/private').DISPLAY} */
+export const DISPLAY = ":0";
+
+/** @type {import('$env/static/private').XDG_CURRENT_DESKTOP} */
+export const XDG_CURRENT_DESKTOP = "ubuntu:GNOME";
+
+/** @type {import('$env/static/private').LANG} */
+export const LANG = "en_US.UTF-8";
+
+/** @type {import('$env/static/private').LC_TELEPHONE} */
+export const LC_TELEPHONE = "fr_FR.UTF-8";
+
+/** @type {import('$env/static/private').XDG_SESSION_DESKTOP} */
+export const XDG_SESSION_DESKTOP = "ubuntu";
+
+/** @type {import('$env/static/private').GNOME_TERMINAL_SERVICE} */
+export const GNOME_TERMINAL_SERVICE = ":1.82";
+
+/** @type {import('$env/static/private').XMODIFIERS} */
+export const XMODIFIERS = "@im=ibus";
+
+/** @type {import('$env/static/private').XAUTHORITY} */
+export const XAUTHORITY = "/run/user/1000/gdm/Xauthority";
+
+/** @type {import('$env/static/private').LS_COLORS} */
+export const LS_COLORS = "rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:";
+
+/** @type {import('$env/static/private').npm_lifecycle_script} */
+export const npm_lifecycle_script = "vite dev";
+
+/** @type {import('$env/static/private').SSH_AUTH_SOCK} */
+export const SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
+
+/** @type {import('$env/static/private').SHELL} */
+export const SHELL = "/usr/bin/zsh";
+
+/** @type {import('$env/static/private').LC_NAME} */
+export const LC_NAME = "fr_FR.UTF-8";
+
+/** @type {import('$env/static/private').npm_package_version} */
+export const npm_package_version = "0.0.1";
+
+/** @type {import('$env/static/private').npm_lifecycle_event} */
+export const npm_lifecycle_event = "dev";
+
+/** @type {import('$env/static/private').QT_ACCESSIBILITY} */
+export const QT_ACCESSIBILITY = "1";
+
+/** @type {import('$env/static/private').GDMSESSION} */
+export const GDMSESSION = "ubuntu";
+
+/** @type {import('$env/static/private').LC_MEASUREMENT} */
+export const LC_MEASUREMENT = "fr_FR.UTF-8";
+
+/** @type {import('$env/static/private').LC_IDENTIFICATION} */
+export const LC_IDENTIFICATION = "fr_FR.UTF-8";
+
+/** @type {import('$env/static/private').GPG_AGENT_INFO} */
+export const GPG_AGENT_INFO = "/run/user/1000/gnupg/S.gpg-agent:0:1";
+
+/** @type {import('$env/static/private').QT_IM_MODULE} */
+export const QT_IM_MODULE = "ibus";
+
+/** @type {import('$env/static/private').npm_config_globalconfig} */
+export const npm_config_globalconfig = "/usr/local/etc/npmrc";
+
+/** @type {import('$env/static/private').npm_config_init_module} */
+export const npm_config_init_module = "/home/le-gonz/.npm-init.js";
+
+/** @type {import('$env/static/private').PWD} */
+export const PWD = "/home/le-gonz/Epitech/tek3/DEV/B-DEV-500-BER-5-1-area-yann-arthur.tcheumani/WebApp";
+
+/** @type {import('$env/static/private').npm_execpath} */
+export const npm_execpath = "/usr/local/lib/node_modules/npm/bin/npm-cli.js";
+
+/** @type {import('$env/static/private').XDG_DATA_DIRS} */
+export const XDG_DATA_DIRS = "/usr/share/ubuntu:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop";
+
+/** @type {import('$env/static/private').XDG_CONFIG_DIRS} */
+export const XDG_CONFIG_DIRS = "/etc/xdg/xdg-ubuntu:/etc/xdg";
+
+/** @type {import('$env/static/private').npm_config_global_prefix} */
+export const npm_config_global_prefix = "/usr/local";
+
+/** @type {import('$env/static/private').LC_NUMERIC} */
+export const LC_NUMERIC = "fr_FR.UTF-8";
+
+/** @type {import('$env/static/private').npm_command} */
+export const npm_command = "run-script";
+
+/** @type {import('$env/static/private').LC_PAPER} */
+export const LC_PAPER = "fr_FR.UTF-8";
+
+/** @type {import('$env/static/private').VTE_VERSION} */
+export const VTE_VERSION = "6003";
+
+/** @type {import('$env/static/private').INIT_CWD} */
+export const INIT_CWD = "/home/le-gonz/Epitech/tek3/DEV/B-DEV-500-BER-5-1-area-yann-arthur.tcheumani/WebApp";
+
+/** @type {import('$env/static/private').EDITOR} */
+export const EDITOR = "vi";
